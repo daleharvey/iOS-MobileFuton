d479285da1ce3c313ee800a6510268aabf604a51
Dale Harvey <dale@arandomurl.com>
2011-09-30 02:45:20 +0100

    Initial Commit

---
+++ MobileFuton/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MobileFuton
+//
+//  Created by Dale Harvey on 30/09/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MobileFuton/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MobileFuton
+//
+//  Created by Dale Harvey on 30/09/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
